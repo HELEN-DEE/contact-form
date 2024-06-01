17d5d56ef67a9d716010391b46c56247f3a5ba80
HELEN-DEE <helendeee12@gmail.com>
2024-06-01 23:03:44 +0100

    Added complete styled components

---
+++ script.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const radioOptions = document.querySelectorAll('.radio-option input[type="radio"]');
+  
+//     radioOptions.forEach(radio => {
+//       radio.addEventListener('change', function() {
+//         // Remove active class from all radio options
+//         radioOptions.forEach(r => r.parentElement.classList.remove('active'));
+        
+//         // Add active class to the parent of the selected radio button
+//         if (radio.checked) {
+//           radio.parentElement.classList.add('active');
+//         }
+//       });
+//     });
+//   });
+
